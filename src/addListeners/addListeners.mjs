8c812d0d3ae339269202daa691c9f9fb774608d5
Pavlo Omelianchuk <pavlotvingo@gmail.com>
2022-03-21 10:23:18 +0000

    Add tab index and onkeypress

--- src/addListeners/addListeners.mjs
+++ src/addListeners/addListeners.mjs
@@ -1,14 +1,22 @@
 import invoke from "./invoke.mjs";
 
 export default function addListeners(self) {
-  let lis = document.querySelectorAll("._access-menu ul li");
+  let functionalidades = document.querySelectorAll("._access-menu ul li");
 
-  for (let i = 0; i < lis.length; i++) {
-    lis[i].addEventListener(
+  for (let i = 0; i < functionalidades.length; i++) {
+    functionalidades[i].addEventListener(
       "click",
       (e) => {
-        let evt = e || window.event;
-        invoke(self, evt.target.getAttribute("data-access-action"));
+        let event = e || window.event;
+        invoke(self, event.target.getAttribute("data-access-action"));
+      },
+      false
+    );
+    functionalidades[i].addEventListener(
+      "keypress",
+      (e) => {
+        let event = e || window.event;
+        invoke(self, event.target.getAttribute("data-access-action"));
       },
       false
     );
